59d7874bea2a18f3b06f803433a5a1acb042572f
florieger <florieger@appcron.com>
2016-06-23 01:31:51 +0200

    Added registerInteractor functionality.

--- Sources/InteractorExecuter.swift
+++ Sources/InteractorExecuter.swift
@@ -1,7 +1,18 @@
 import Foundation
 
 public class InteractorExecuter {
-    public func execute<RequestType, ResponseType>(request:RequestType) -> ResponseType {
+    var interactors = Dictionary<String, AnyObject>()
+    
+    public func execute<RequestType>(request:RequestType) {
         //TODO: Make magic happen
+        
+        let interactor: AnyObject = interactors[String(request)]!
+ 
+        print("%@", interactor)
+    }
+    
+    public func registerInteractor<InteractorType: Interactor>(interactor: InteractorType, request: AnyObject) {
+        let key = String(request)
+        interactors[key] = interactor
     }
 }
