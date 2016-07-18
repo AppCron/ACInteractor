a0a89afcee6843c080a296dd4e76e74aa6453442
florieger <florieger@appcron.com>
2016-07-18 13:10:43 +0200

    Implemented getInteractor() function in LazyInteractor.

--- Sources/LazyInteractor.swift
+++ Sources/LazyInteractor.swift
@@ -3,9 +3,21 @@ import Foundation
 class LazyInteractor<InteractorType: Interactor>: Interactor {
     
     private(set) var lazyInstance: InteractorType?
+    private let factory: (Void -> InteractorType)
     
     init(factory:(Void -> InteractorType)) {
+        self.factory = factory
+    }
+    
+    func getInteractor() -> InteractorType {
+        if let instance = lazyInstance {
+            return instance
+        }
         
+        let instance = factory()
+        self.lazyInstance = instance
+        
+        return instance
     }
     
     func execute(request: InteractorType.Request) {
--- Tests/LazyInteractorTests.swift
+++ Tests/LazyInteractorTests.swift
@@ -3,25 +3,46 @@ import XCTest
 
 class LazyInteractorTests: XCTestCase {
     
+    let testDependency = "testDependency"
+    let testFactory = {return TestInteractor(dependency: "testDependency")}
+    var lazyInteractor: LazyInteractor<TestInteractor>!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        lazyInteractor = LazyInteractor(factory: testFactory)
     }
     
-    // MARK: Init
+    // MARK: init()
     
     func testInit_doesNotInitializeLazyInstance()
     {
-        // Arrange
-        let factory = {return TestInteractor(dependency: "")}
-        
-        // Act
-        let lazyInteractor = LazyInteractor(factory: factory)
-        
         // Assert
         XCTAssertNil(lazyInteractor.lazyInstance)
     }
     
+    // MARK: getInteractor()
+    
+    func testGetInteractor_returnsInstanceBuiltWithFactory()
+    {
+        // Act
+        let interactor = lazyInteractor.getInteractor()
+        
+        // Assert
+        XCTAssertEqual(interactor.dependency, testDependency)
+    }
+    
+    
+    func testGetInteractor_calledTwice_doesNotCreateNewInstance()
+    {
+        // Act
+        let firstInteractor = lazyInteractor.getInteractor()
+        let secondInteractor = lazyInteractor.getInteractor()
+        
+        // Assert
+        XCTAssert(firstInteractor === secondInteractor)
+    }
     
     
     // MARK: Test Interactors
