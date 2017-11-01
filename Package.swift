74f027a06d06b2ec411cbc60363a53448827363d
Florian Rieger <florieger@appcron.com>
2017-11-01 11:54:00 +0100

    Adjusted Package.swift for PackageDescription API Version 4.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,21 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "ACInteractor",
-    dependencies : []
+    products: [
+        .library(
+            name: "ACInteractor",
+            targets: ["ACInteractor"]),
+        ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ACInteractor",
+            dependencies: []),
+        .testTarget(
+            name: "ACInteractorTests",
+            dependencies: ["ACInteractor"]),
+        ]
 )
